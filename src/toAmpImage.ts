02d9fa9a6d54cf29607d4ba580ce4598b41ec35d
illumination-k <illumination.k.27@gmail.com>
2022-04-07 03:43:54 +0900

    replace pattern of links

--- src/toAmpImage.ts
+++ src/toAmpImage.ts
@@ -8,15 +8,29 @@ import { Plugin } from "unified";
 type Option = {
   defaultWidth: number;
   defaultHeight: number;
+  replacePattern: { pattern: RegExp; to: string } | undefined;
 };
 
 export default function toAmpImage(
-  option: Option = { defaultHeight: 100, defaultWidth: 100 }
+  option: Option = {
+    defaultHeight: 100,
+    defaultWidth: 100,
+    replacePattern: undefined,
+  }
 ): Plugin {
   return (ast: Parent) => {
     visit(ast, "image", (node: Image, index: number, parent: Parent) => {
-      const url = node.url;
+      let url = node.url;
       const alt = node.alt || "No alt";
+
+      if (option.replacePattern) {
+        if (option.replacePattern.pattern.test(url)) {
+          url = url.replace(
+            option.replacePattern.pattern,
+            option.replacePattern.to
+          );
+        }
+      }
 
       const buf = Buffer.from(sr("GET", url).getBody());
       const dimensions = sizeOf(buf);
